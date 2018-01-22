a974c2518b4287f7ca82a9c92173a52d85eeb116
sowee121 <609997400@qq.com>
2018-01-22 10:10:25 +0800

    company commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import 'element-theme-default';
 
 import MyNav from './components/navMenu';
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'element-theme-default';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
